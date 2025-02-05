7b8a0c9b46d77cdd864d9b740af7eee4313d6870
Minnnning <6alswjd6@gmail.com>
2025-02-05 19:07:46 +0900

    feat: fetchuser api 추가

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -8,6 +8,7 @@ class AuthService {
   final Dio _dio = Dio();
   final FlutterSecureStorage _secureStorage = const FlutterSecureStorage(); 
 
+  // 카카오 로그인
   Future<UserModel?> login(UserProvider userProvider) async {
     try {
       OAuthToken token;
@@ -35,7 +36,14 @@ class AuthService {
 
         UserModel user = UserModel.fromJson(data);
         userProvider.setUser(user); // ✅ 유저 정보 저장
-
+        // try {
+        //   var a = await _secureStorage.read(key: 'accessToken');
+        //   var b = await _secureStorage.read(key: 'refreshToken');
+        //   print(a);
+        //   print(b);
+        // } catch(error) {
+        //   print(error);
+        // }
         print('서버 로그인 성공, 토큰 저장 완료');
         return user;
       } else {
@@ -48,6 +56,46 @@ class AuthService {
     }
   }
 
+  // 유저 정보 가져오기
+  Future<UserModel?> fetchUser(UserProvider userProvider) async {
+    try {
+      String? accessToken = await _secureStorage.read(key: 'accessToken');
+      if (accessToken == null) {
+        print('액세스 토큰 없음, 로그인 필요');
+        return null;
+      }
+
+      final response = await _dio.get(
+        'https://api.mapping.kro.kr/api/v2/member/user-info',
+        options: Options(headers: {
+          'Authorization': 'Bearer $accessToken',
+          'accept': 'application/json',
+        }),
+      );
+
+      if (response.statusCode == 200 && response.data['success']) {
+        final data = response.data['data'];
+        UserModel user = UserModel(
+          nickname: data['nickname'],
+          profileImageUrl: data['profileImage'],
+          role: data['role'],
+          socialId: data['socialId'],
+        );
+
+        userProvider.setUser(user);
+        print('유저 정보 갱신 성공');
+        return user;
+      } else {
+        print('유저 정보 가져오기 실패: ${response.data}');
+        return null;
+      }
+    } catch (error) {
+      print('유저 정보 요청 중 오류 발생: $error');
+      return null;
+    }
+  }
+
+  // 로그아웃
   Future<void> logout(UserProvider userProvider) async {
     try {
       await _secureStorage.delete(key: 'accessToken');
