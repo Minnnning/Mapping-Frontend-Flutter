1b59c47a3f792e542dd4eeb4b2ca3ef69f15b9d6
minjeong kim <6alswjd6@gmail.com>
2025-03-20 21:03:14 +0900

    fix: 화장실 필터 수정

--- lib/screens/category_bar.dart
+++ lib/screens/category_bar.dart
@@ -6,14 +6,15 @@ import '../providers/marker_provider.dart';
 class CategoryBar extends StatelessWidget {
   const CategoryBar({Key? key}) : super(key: key);
 
-  final List<String> categories = const [
-    "전체",
-    "화장실",
-    "주차장",
-    "흡연장",
-    "쓰레기통",
-    "기타"
-  ];
+  // 표시할 카테고리와 실제 카테고리를 매핑
+  final Map<String, String> categoryMapping = const {
+    "전체": "전체",
+    "화장실": "공용 화장실",
+    "주차장": "주차장",
+    "흡연장": "흡연장",
+    "쓰레기통": "쓰레기통",
+    "기타": "기타",
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +25,17 @@ class CategoryBar extends StatelessWidget {
           child: ListView.separated(
             scrollDirection: Axis.horizontal,
             padding: const EdgeInsets.symmetric(horizontal: 16),
-            itemCount: categories.length,
+            itemCount: categoryMapping.keys.length,
             separatorBuilder: (context, index) => const SizedBox(width: 8),
             itemBuilder: (context, index) {
-              String category = categories[index];
-              bool isSelected = markerProvider.selectedCategory == category;
+              String displayCategory = categoryMapping.keys.elementAt(index);
+              String actualCategory = categoryMapping[displayCategory]!;
+              bool isSelected =
+                  markerProvider.selectedCategory == actualCategory;
 
               return ChoiceChip(
                 label: Text(
-                  category,
+                  displayCategory,
                   style: TextStyle(
                     color: isSelected ? Colors.white : Colors.black,
                     fontWeight: FontWeight.bold,
@@ -41,7 +44,7 @@ class CategoryBar extends StatelessWidget {
                 selected: isSelected,
                 onSelected: (selected) {
                   if (selected) {
-                    markerProvider.setCategory(category);
+                    markerProvider.setCategory(actualCategory);
                   }
                 },
                 selectedColor: mainColor,
