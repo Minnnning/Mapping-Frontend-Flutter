879df9e5401ac91c4c18af5017c9721ad84651fb
minjeong kim <6alswjd6@gmail.com>
2025-04-05 00:08:59 +0900

    feat: 마커 리스트 검색시 토큰 이용

--- lib/services/marker_service.dart
+++ lib/services/marker_service.dart
@@ -1,16 +1,37 @@
 import 'dart:convert';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class MarkerService {
+  static final FlutterSecureStorage _secureStorage =
+      const FlutterSecureStorage();
+
+  /// 엑세스 토큰을 가져오는 함수
+  static Future<String?> _getAccessToken() async {
+    return await _secureStorage.read(key: 'accessToken');
+  }
+
   static Future<Set<Marker>> fetchMarkers(
       LatLng center, Function(Map<String, dynamic>) onTap) async {
     final String url =
         'https://api.mapping.kro.kr/api/v2/memo/total?lat=${center.latitude}&lng=${center.longitude}&km=5';
 
     try {
-      final response =
-          await http.get(Uri.parse(url), headers: {'accept': '*/*'});
+      // 🔥 토큰 가져오기
+      String? token = await _getAccessToken();
+
+      // 기본 헤더 설정
+      Map<String, String> headers = {'accept': '*/*'};
+
+      // 토큰이 있다면 Authorization 헤더 추가
+      if (token != null) {
+        headers['Authorization'] = 'Bearer $token';
+      } else {
+        print("⚠️ 엑세스 토큰 없음. 토큰 없이 요청을 보냅니다.");
+      }
+
+      final response = await http.get(Uri.parse(url), headers: headers);
 
       if (response.statusCode == 200) {
         final Map<String, dynamic> data =
